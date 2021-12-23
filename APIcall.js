0d85b82b281493fc9d0d9df2fdb4429ddcaca6f1
Stevie Ambroise <s.ambroise@protonmail.com>
2021-12-22 21:32:30 -0700

    Move fetch request from API file to main file

--- APIcall.js
+++ APIcall.js
@@ -1,5 +1,5 @@
 export const getAPIdata = () => {
-  fetch('https://mysterious-mesa-00016.herokuapp.com/items')
+  return fetch('https://mysterious-mesa-00016.herokuapp.com/items')
   .then(response => response.json())
   .catch(err => console.log(err))
 }
